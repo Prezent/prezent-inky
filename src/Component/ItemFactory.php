3c0383a7783f8a31b6748b3e1e12ddbf100f007e
Sander Marechal <sander@prezent.nl>
2018-06-18 10:29:47 +0200

    Item component

---
+++ src/Component/ItemFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Prezent\Inky\Component;
+
+/**
+ * @author Sander Marechal
+ */
+class ItemFactory extends BaseFactory
+{
+    protected static $template = <<<EOL
+<th class="menu-item"><a href="#"><inky-content /></a></th>
+EOL;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getName(): string
+    {
+        return 'item';
+    }
+
+    public function parse(\DOMNode $target)
+    {
+        $href = $target->hasAttribute('href') ? $target->getAttribute('href') : '#';
+        $class = $target->hasAttribute('class') ? $target->getAttribute('class') : null;
+
+        $th = $this->replace($target, self::$template);
+
+        if ($class) {
+            $this->addClass($th, $class);
+        }
+
+        $th->firstChild->setAttribute('href', $href);
+    }
+}
---
+++ tests/Component/ItemFactoryTest.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Prezent\Inky\Tests\Component;
+
+use Prezent\Inky\Component\ItemFactory;
+use Symfony\Component\DomCrawler\Crawler;
+
+/**
+ * @author Sander Marechal
+ */
+class ItemFactoryTest extends ComponentTestCase
+{
+    public function testBasic()
+    {
+        $crawler = $this->createCrawler('<menu><item href="/foo">item</item></menu>');
+        $factory = new ItemFactory();
+
+        foreach ($crawler->filter('item') as $node) {
+            $factory->parse($node);
+        }
+
+        $this->assertCount(1, $crawler->filter('menu > th.menu-item > a'));
+        $this->assertEquals('/foo', $crawler->filter('menu > th.menu-item > a')->attr('href'));
+    }
+
+    public function testDefaultHref()
+    {
+        $crawler = $this->createCrawler('<menu><item>item</item></menu>');
+        $factory = new ItemFactory();
+
+        foreach ($crawler->filter('item') as $node) {
+            $factory->parse($node);
+        }
+
+        $this->assertCount(1, $crawler->filter('menu > th.menu-item > a'));
+        $this->assertEquals('#', $crawler->filter('menu > th.menu-item > a')->attr('href'));
+    }
+
+    public function testClasses()
+    {
+        $crawler = $this->createCrawler('<menu><item href="/foo" class="bar">item</item></menu>');
+        $factory = new ItemFactory();
+
+        foreach ($crawler->filter('item') as $node) {
+            $factory->parse($node);
+        }
+
+        echo $crawler->html();
+
+        $this->assertCount(1, $crawler->filter('menu > th.menu-item.bar > a'));
+    }
+}
